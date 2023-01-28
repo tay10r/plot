7ffcd73590944160d6b4bd6e5a0e72f903f1b87e
Taylor Holberton <taylor.holberton@protonmail.com>
2023-01-28 03:37:22 -0500

    Added windows entry point.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
-int main() { return 0; }
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
+#ifdef _WIN32
+int
+WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+#else
+int
+main()
+#endif
+{
+  return 0;
+}
